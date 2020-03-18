95141f3fdb466de80722c752ed32701e614ce7ba
LazarVujosevic <lazar.vujosevic100@gmail.com>
2020-03-18 02:13:39 +0100

    Dodat JSON file sa podacima o proizvodima
    Dodate Business Object i View model klase.
    Dodata funkcionalnost uzimanja podataka iz JSON file-a i slanja na View

---
+++ Models/JsonProizvodiViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proizvodi.Models
+{
+    public class JsonProizvodiViewModel
+    {
+        public List<JsonProizvodiBaseViewModel> proizvodi = new List<JsonProizvodiBaseViewModel>();
+    }
+
+}
